78da6a6a824e07a81325a0c90004b5fb9bbe244b
Javier Wilson <javier.wilson@gmail.com>
2016-04-08 04:00:11 -0600

    acutalizado para que funcione con django 1.8

--- hero_slider/static/hero_slider/js/generic_raw_id_field.js
+++ hero_slider/static/hero_slider/js/generic_raw_id_field.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
                 function(data) {
                     $('#lookup_box').html('<a id="lookup_id_object_id" class="related-lookup" onclick="return showRelatedObjectLookupPopup(this);"></a>');
                     $('#lookup_id_object_id').html('<img src="'+ static_url + 'admin/img/selector-search.gif"/> Lookup ' + type);
-                    $('#lookup_id_object_id').attr('href', admin_url + data.app_label +'/'+ data.model +'/?t=id');
+                    $('#lookup_id_object_id').attr('href', admin_url + data.app_label +'/'+ data.model +'/?_to_field=id&_popup=1');
                 }
             )
         }
